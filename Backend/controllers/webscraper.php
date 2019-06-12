986e82da1033cc35f3e4ff78787415da4fcc6e62
Deedss <Deedss@github.com>
2019-06-12 10:12:29 +0200

    webscraper to pdo

--- Backend/controllers/webscraper.php
+++ Backend/controllers/webscraper.php
@@ -1,47 +1,64 @@
 <?php
 	include_once("simple_html_dom.php");
+class webscraperController{
 
-	if(!empty($_POST['search'])){
+	public function getShelfs(){
+		$this->db = (new Database())->connect();
+        $sql = $this->db->prepare("SELECT * FROM shelf");
+        $status = $sql->execute(array());
+        if (!$status) {
+            echo "Something went wrong. Please try again later";
+            die();
+        }
+        $books = $sql->fetchall(DBO::FETCH_ASSOC);
 
-		$search = $_POST['search'];
+        echo $books;
+	}
 
-		$rss = 'http://lbs-nhl.oclc.org:8080//psi_rss/rss_feeds.php?DB=1.5&SEARCH=00yS!i1016!t'. $search .'!aY!cN.oY.vD.wD';
-        $html = file_get_html($rss);		
+	public function getResults(){
+		if(!empty($_POST['search'])){
 
-		$itemArray = $html->find('item');	
-		$titleArray = $html->find('item title');
-		$descriptionArray = $html->find('item description');
-		$max = sizeof($titleArray);
-
-
-		for($i=0; $i < $max; $i++){
-			$book = substr($itemArray[$i]->plaintext, 0, 56);
-			$htmlBook = file_get_html($book);
-
-			$value = "";
-			$bookArray = $htmlBook->find('div.lrmargin div table tr td.rec_title div span');
-			$sizeBookArray = sizeof($bookArray);
-			$regex = "/Emmen /";
-			foreach($bookArray as $element){
-				if(preg_match($regex ,$element->innertext)){
-					$value = explode(" ", $element->innertext);
-					$number = $value[1] . " " . $value[2];
-					break;
+			$search = $_POST['search'];
+	
+			$rss = 'http://lbs-nhl.oclc.org:8080//psi_rss/rss_feeds.php?DB=1.5&SEARCH=00yS!i1016!t'. $search .'!aY!cN.oY.vD.wD';
+			$html = file_get_html($rss);		
+	
+			$itemArray = $html->find('item');	
+			$titleArray = $html->find('item title');
+			$descriptionArray = $html->find('item description');
+			$max = sizeof($titleArray);
+	
+	
+			for($i=0; $i < $max; $i++){
+				$book = substr($itemArray[$i]->plaintext, 0, 56);
+				$htmlBook = file_get_html($book);
+	
+				$value = "";
+				$bookArray = $htmlBook->find('div.lrmargin div table tr td.rec_title div span');
+				$sizeBookArray = sizeof($bookArray);
+				$regex = "/Emmen /";
+				foreach($bookArray as $element){
+					if(preg_match($regex ,$element->innertext)){
+						$value = explode(" ", $element->innertext);
+						$number = $value[1] . " " . $value[2];
+						break;
+					}
 				}
+	
+				$reserverdArray = $htmlBook->find('div  ');
+				$title = $titleArray[$i]->plaintext;
+				$description = $descriptionArray[$i]->plaintext;
+	
+				$values[] = array(
+					'number' => $number,
+					'title' => $title,
+					'description' => $description
+				);
 			}
-
-			$reserverdArray = $htmlBook->find('div  ');
-			$title = $titleArray[$i]->plaintext;
-			$description = $descriptionArray[$i]->plaintext;
-
-			$values[] = array(
-				'number' => $number,
-				'title' => $title,
-				'description' => $description
-			);
-		}
-		
-		$json = json_encode($values);
-		echo $json;
-	}	
+			
+			$json = json_encode($values);
+			echo $json;
+		}	
+	}
+}	
 ?>
